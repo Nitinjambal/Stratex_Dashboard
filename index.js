7a44a6844500c390da319db78a3e6c7cd6b53dac
nitin <nitinjam8800@gmail.com>
2023-09-11 00:49:39 +0530

    updated cors origins

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"]
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
 }));
